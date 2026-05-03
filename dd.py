e748731f472ddc6209a76ec0490524eec9764712
14HOssEIN14 <121733269+14HOssEIN14@users.noreply.github.com>
2026-05-03 10:19:23 +0330

    Update dd.py

--- dd.py
+++ dd.py
@@ -1,13 +1,22 @@
 import requests
 import os
+import tarfile
+import shutil
 
 url = "https://sourceforge.net/projects/biosig/files/BioSig%20for%20Octave%20and%20Matlab/biosig4octmat-latest.tar.gz/download"
-
 headers = {'User-Agent': 'Mozilla/5.0'}
 
-response = requests.get(url, headers=headers, allow_redirects=True)
+print("Downloading...")
+r = requests.get(url, headers=headers, allow_redirects=True)
 
-with open("biosig4octmat-latest.tar.gz", "wb") as f:
-    f.write(response.content)
+# ذخیره توی خود ریپازیتوری
+with open("biosig.tar.gz", "wb") as f:
+    f.write(r.content)
 
-print("Done! File size:", os.path.getsize("biosig4octmat-latest.tar.gz"), "bytes")
+# اکسترکت توی خود ریپازیتوری
+print("Extracting...")
+with tarfile.open("biosig.tar.gz", "r:gz") as tar:
+    tar.extractall(".")
+
+print("Done! فایل‌ها توی ریپازیتوری هستند:")
+print(os.listdir("."))
